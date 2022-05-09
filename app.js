e9e8870215326bb7f8904a67f60819a55b52be5d
XCA Student <xcastudent@Admins-MBP.attlocal.net>
2022-05-09 15:20:55 -0400

    added objects.js file

---
+++ app.js
@@ -0,0 +1,158 @@
+
+
+
+let hangmanTries = 6
+let hangmanAttepmts = 0
+let objectArray = []
+
+const letterbutton = document.querySelector('#alphabet')
+const buttons = document.querySelectorAll('#alphabet button')
+const hintButton = document.getElementById('hint')
+const startButton = document.getElementById('play-again')
+const triesLeft = document.querySelector('#tries-left')
+const phraseUl = document.querySelector('#word-guess ul')
+const subject = document.getElementById('subject')
+const hintDiv = document.querySelector('.hint-div')
+const hangmanPic = document.getElementById('hangmanPic')
+const gameArea = document.querySelector('.game-play-area')
+const startPage = document.getElementById('startPage')
+// triesLeft.innerHTML = `Tries left: ${hangmanTries}`
+
+function randomObject(arr) {
+    const i = Math.floor(Math.random() * arr.length)
+    const object = arr[i]
+    console.log(object)
+    return object
+}
+
+function setupGame() {
+    gameArea.style.display = 'none'
+    startPage.style.display = 'block'
+    objectArray = []
+}
+
+setupGame()
+
+function startGame() {
+    gameArea.style.display = 'block'
+    startPage.style.display = 'none'
+    hangmanTries = 6
+    hangmanAttepmts = 0
+    hangmanPic.src = `./images/${hangmanAttepmts}.jpg`
+    hintDiv.innerHTML = ''
+    triesLeft.innerHTML = `Tries left: ${hangmanTries}`
+    buttons.forEach(button => {
+        button.disabled = false
+    })
+    const chosenObject = randomObject(objectArray)
+    console.log(chosenObject.title)
+    const wordArray = Array.from(chosenObject.title)
+    addPhraseToDisplay(wordArray, chosenObject.category)
+    chosenTitle = wordArray
+    chosenHint = chosenObject.hint
+}
+
+let chosenTitle;
+let chosenHint;
+// startGame()
+console.log(chosenTitle)
+
+// const category = object.category
+//     const hint = object.hint
+//     const randomWord = Array.from(object.title)
+// const wordArray = 
+
+function addPhraseToDisplay(arr, arr2) {
+    arr.forEach(item => {
+        let letter = document.createTextNode(item);
+        let li = document.createElement('li');
+        li.append(letter);
+        phraseUl.append(li);
+    
+        if (li.textContent === " ") {
+            li.className = 'space';
+        } else {
+            li.className = 'letter';
+        }
+        return li
+    })
+    subject.innerHTML = `Catergory: ${arr2}`
+    
+};
+
+
+function displayLetters(letter) {
+    const elementsLi = document.getElementsByClassName('letter')
+    for (let i = 0; i < elementsLi.length; i++) {
+        const li = elementsLi[i]
+        if(li.textContent.toLocaleLowerCase() === letter) {
+            li.classList.add('show')
+        }
+    }
+}
+
+function checkWin() {
+    const letter = document.getElementsByClassName('letter');
+    const show = document.getElementsByClassName('show');
+
+    if(letter.length === show.length) {
+        alert("You Win!!")
+    } else if(hangmanAttepmts === 6) {
+        alert("You Lost!")
+        console.log(buttons)
+        buttons.forEach(button => {
+            button.disabled = true
+        })
+    }
+}
+
+hintButton.addEventListener('click', (e) => {
+    hintDiv.innerHTML = chosenHint
+})
+
+startButton.addEventListener('click', (e) => {
+    let child = phraseUl.lastElementChild
+    while(child) {
+        phraseUl.removeChild(child)
+        child = phraseUl.lastElementChild
+    }
+    setupGame()
+})
+
+const array = []
+
+letterbutton.addEventListener('click', (e) => {
+    const letter = e.target.innerHTML.toLowerCase()
+    console.log(letter)
+    chosenTitle.forEach(digit => {
+        digit = digit.toLowerCase()
+        if(letter === digit) {
+            console.log(digit)
+            displayLetters(letter)
+            array.push(digit)
+            console.log(array)
+        } else {
+            console.log(digit.toUpperCase())
+        }
+    })
+    if(!array.includes(letter)) {
+        hangmanAttepmts++
+        hangmanPic.src = `./images/${hangmanAttepmts}.jpg`
+
+        hangmanTries--
+        triesLeft.innerHTML = `Tries left: ${hangmanTries}`
+    }
+    checkWin()
+})
+
+startPage.addEventListener('click', (e) => {
+    let category = e.target.innerHTML
+    console.log(category)
+    wordsObject.forEach(object => {
+        if(category === object.category) {
+            objectArray.push(object)
+        }
+    })
+    console.log(objectArray)
+    startGame()
+})
---
+++ objects.js
@@ -0,0 +1,72 @@
+const wordsObject = [
+        {
+            "title": "Jurassic Park",
+            "category": "Movies",
+            "hint": "A movie about 'Dino DNA'"
+        },
+        {
+            "title": "Banana",
+            "category": "Food",
+            "hint": "It a fruit."
+        },
+        {
+            "title": "Macbook Pro",
+            "category": "Computer",
+            "hint": "Apple"
+        },
+        {
+            "title": "The Beatles",
+            "category": "Music",
+            "hint": "Like a bug"
+        },
+        {
+            "title": "Bobbys World",
+            "category": "Cartoon",
+            "hint": "Howie Mandel as a child"
+        },
+        {
+            "title": "Surfing",
+            "category": "Sport",
+            "hint": "Originated from Hawaii"
+        },
+        {
+            "title": "Double Bass",
+            "category": "Music",
+            "hint": "4 Strings"
+        },
+        {
+            "title": "Miles Davis",
+            "category": "Music",
+            "hint": "Nicknamed the Prince of Darkness"
+        },
+        {
+            "title": "Pepsi Cola",
+            "category": "Food",
+            "hint": "In a blue can."
+        },
+        {
+            "title": "Air Jordan",
+            "category": "Clothes",
+            "hint": "Nike"
+        },
+        {
+            "title": "Titanic",
+            "category": "Movies",
+            "hint": "I'll Never Let GO, Jack"
+        },
+        {
+            "title": "Baseball",
+            "category": "Sport",
+            "hint": "They have innings"
+        },
+        {
+            "title": "Microsoft",
+            "category": "Computer",
+            "hint": "Bill Gates"
+        },
+        {
+            "title": "A Tribe Called Quest",
+            "category": "Music",
+            "hint": "90's Hip Hop"
+        }
+    ]
